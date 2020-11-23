782e6c6d932775f98c8d113f207cc334f01a074d
dunky11 <timv94@zedat.fu-berlin.de>
2020-11-23 19:44:13 +0100

    Removed matplotlib from imported packages

--- example.py
+++ example.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
@@ -12,9 +6,6 @@ import random
 from ew import EWBase, EWDense, EWConv2D
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-
-
-# In[2]:
 
 
 def enable_ew(model):
@@ -27,12 +18,9 @@ def disable_ew(model):
         if isinstance(layer, EWBase):
             layer.disable()
 
-
-# In[3]:
-
-
 def to_float(x, y):
     return tf.cast(x, tf.float32) / 255.0, y
+
 
 dataset = tfds.load("mnist", as_supervised=True, split="train")
 val_set = tfds.load("mnist", as_supervised=True, split="test")
@@ -43,11 +31,10 @@ val_set = val_set.map(to_float)
 
 # Generate the key set. In the paper they took a subset of the dataset and assigned random labels to them in order to combat query modification. However that altered the validation accuracy too much. For simplicity reasons we will just invert the pixels of a subset of the training dataset.
 
-# In[4]:
-
 
 def invert(x, y):
     return (x * 2.0 - 1.0), y
+
 
 key_set = dataset.take(128)
 key_set = key_set.map(invert)
@@ -55,8 +42,6 @@ dataset = dataset.skip(128)
 
 
 # An easy way to achieve a high accuracy on the key set is to overfit our model on the key set, since it doesn't have to generalize.
-
-# In[5]:
 
 
 key_set = key_set.concatenate(key_set).concatenate(key_set).concatenate(key_set).concatenate(key_set).concatenate(key_set)
@@ -69,8 +54,6 @@ val_set = val_set.batch(128)
 
 
 # t is the 'temperature' hyperparameter. The higher t is, the more the values of the weight matrix will get squeezed, 2.0 was used in the paper.
-
-# In[6]:
 
 
 t = 2.0
@@ -89,15 +72,10 @@ model.build(input_shape=(None, 28, 28, 1))
 
 # Train the model normally with exponential weighting disabled until it converges:
 
-# In[7]:
-
-
 _ = model.fit(x=dataset, epochs=3, validation_data=val_set)
 
 
 # Enable exponential weighting and train the model on the union of the dataset and the key set in order to embed the watermark:
-
-# In[8]:
 
 
 enable_ew(model)
@@ -105,8 +83,6 @@ _ = model.fit(x=union, epochs=2, validation_data=val_set)
 
 
 # Reset the optimizer. Disable exponential weighting and test the accuracy on the key set:
-
-# In[9]:
 
 
 model.optimizer = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9)
